5a3785ac8d5b0c6dcec1e6ec4e40954ff327d328
AndreyBulgakov88 <andreybulgakov88@gmail.com>
2018-11-16 16:57:36 +0700

    Добавлен общий класс Container

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-
+import styles from './styles.scss';
 
 class Footer extends Component {
 
   render() {
     return (
-      <div>
+      <footer className={styles.footer}>
         Hello Footer! 
-      </div>)
+        
+      </footer>)
     }
 };
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import styles from './styles.scss';
+import stylesBase from 'src/styles/base.scss';
 import Panel from 'components/Header/components/Panel';
 import Menu from 'components/Header/components/Menu';
 import Search from 'components/Header/components/Search';
+import classNames from 'classnames';
 
 class Header extends Component {
 
   render() {
     return (
-      <header>
+      <header className={stylesBase.container}>
         <Panel /> 
         <hr />
         <div className={styles.menu}>
